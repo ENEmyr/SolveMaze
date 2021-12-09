e7c13ece2575bd9ed62053110276fec13b753c83
ENEmy <nosde@protonmail.com>
2021-12-09 09:02:33 +0700

    feat: finish Mapper class, finish unstable main file

--- main.py
+++ main.py
@@ -1,36 +1,167 @@
-from time import sleep
+import subprocess, optparse
 import matplotlib.pyplot as plt
+import numpy as np
 from time import sleep
+from time import sleep
+from rich import pretty
 from rich.console import Console
 from rich.traceback import install
+from rich.progress import track
 from src.Client import Client
 from src.Odometry import Odometry
 from src.Mapper import Mapper
 
 console = Console()
+pretty.install()
 install() # install rich traceback
 
-def main():
-    block_length = 400
+def run(
+        solve_mode:bool = True,
+        maze_size:int = 6,
+        block_length:int = 400,
+        tick_rate:float = .1,
+        show_maze:bool = True,
+        dynamic_update_maze:bool = False,
+        debug_mode:bool = False
+        ):
+    # TODO: dynamictically update explored map, fix solver
     client = Client()
-    mapper = Mapper(maze_size=12, padd=True)
+    mapper = Mapper(maze_size=maze_size, padd=True)
     odo = Odometry()
     client.loop_start()
+    _entrance_exit = None
+    console.print('\nEstasblishing connection to MQTT Broker\n', style='bold cyan')
+    for step in track(range(5)):
+        sleep(.1)
+    else:
+        client.pub({'AngularDistance':'','StopExplore':'','EntranceExit':'','Command':''})
     try:
-        while True:
-            ret = client.sub(['AngularDistance', 'StopExplore'])
-            if ret['StopExplore'] != None:
-                break
-            if ret['AngularDistance'] != None:
-                forward_distance, rotate_angle = odo.maximum_forward_distance(ret['AngularDistance'])
-                mapper.map(forward_distance, rotate_angle, block_length)
-            sleep(.5)
+        # console.print('Step 1: Mapping\n', style='bold cyan')
+        with console.status("[bold cyan]Step 1: Mapping") as status:
+            while True:
+                ret = client.sub(['AngularDistance', 'StopExplore'])
+                if ret['StopExplore'] != None:
+                    console.print(' > Stop Mapping', style='red')
+                    client.pub({'StopExplore': ''})
+                    sleep(.2)
+                    if debug_mode:
+                        console.log('Stop Exploring', log_locals=True)
+                    break
+                if ret['AngularDistance'] != None:
+                    console.print(' > Mapping Local...', style='yellow')
+                    client.pub({'AngularDistance': ''})
+                    sleep(.2)
+                    if debug_mode:
+                        console.log('Put Map', log_locals=True)
+                    forward_distance, rotate_angle = odo.minimum_forward_distance(ret['AngularDistance'])
+                    mapper.map(forward_distance, rotate_angle, block_length)
+                sleep(tick_rate)
     except KeyboardInterrupt:
-        pass
-    maze = mapper.construct()
-    plt.axis('off')
-    plt.imshow(maze, cmap='Greys', interpolation='none')
-    plt.waitforbuttonpress()
+        client.pub({'AngularDistance':'','StopExplore':'','EntranceExit':'','Command':''}, verbose=debug_mode)
+    with console.status("[bold cyan]Step 2: Constructing a maze") as status:
+    # console.print('Step 2: Contructing a maze\n', style='bold cyan')
+        maze = mapper.construct()
+        max_col = maze.shape[0]
+        if show_maze:
+            # plt.axis('off')
+            plt.imshow(maze, cmap='Greys', interpolation='none')
+            plt.savefig('Maze.png')
+            plt.waitforbuttonpress()
+            if debug_mode:
+                console.log('Map Contructed', log_locals=True)
+    if solve_mode:
+        try:
+            with console.status("[bold cyan]Step 3: Waiting for define an entrnace and exit of the maze") as status:
+            # console.print('Step 3: Waiting for define entrance and exit of the maze\n', style='bold cyan')
+                while True:
+                    ret = client.sub(['EntranceExit'])
+                    if ret['EntranceExit'] != None:
+                        if debug_mode:
+                            console.log('Got Entrance and Exit', log_locals=True)
+                        console.print(' > Entrance & Exit was Defined', style='green')
+                        _entrance_exit = ret['EntranceExit'].split('|') # in form : aa, bb | cc, dd
+                        client.pub({'EntranceExit': ''})
+                        sleep(.2)
+                        break
+                    else:
+                        if debug_mode:
+                            console.log(ret, log_locals=True)
+                    sleep(tick_rate)
+        except KeyboardInterrupt:
+            client.pub({'AngularDistance':'','StopExplore':'','EntranceExit':'','Command':''}, verbose=debug_mode)
+            with open('constructed_maze.npy', 'wb') as f:
+                np.save(f, maze, allow_pickle=True)
+        with console.status("[bold cyan]Step 4: Solve the maze") as status:
+        # console.print('Step 4: Solve the maze\n', style='bold cyan')
+            _entrance = list(map(lambda x: int(x), _entrance_exit[0].split(',')))
+            _exit = list(map(lambda x: int(x), _entrance_exit[1].split(',')))
+            maze = np.where(maze>0, -1, maze)
+            maze = np.where(maze==0, 1, maze)
+            maze = np.where(maze==-1, 0, maze).flatten().astype('int')
+            maze_str = ''
+            i = 0
+            row = 0
+            col = 0
+            for x in maze:
+                if row == _entrance[0] and col == _entrance[1]:
+                    maze_str += 's'
+                elif row == _exit[0] and col == _exit[1]:
+                    maze_str += 'f'
+                else:
+                    maze_str += f'{x}'
+                i += 1
+                col += 1
+                if i == max_col:
+                    maze_str += '\n'
+                    i = 0
+                    row += 1
+                    col = 0
+            _entrance = ','.join([str(x) for x in _entrance])
+            if debug_mode:
+                console.print('Maze Map :\n', maze_str)
+            p = subprocess.Popen(['node', './solver.js', f'--maze={maze_str}', f'--entrance={_entrance}'], stdout=subprocess.PIPE)
+            output = p.stdout.read()
+            commands = output.decode('utf-8').replace('\n', '').split(',')
+            commands = commands[::-1] # transform queue -> stack
+            latest_command = ''
+            if debug_mode:
+                console.log('Solved', log_locals=True)
+        try:
+            with console.status("[bold cyan]Step 5: Commanding Robot") as status:
+            # console.print('Step 5: Commanding Robot', style='bold cyan')
+                while True:
+                    ret = client.sub(['Command'])
+                    if ret['Command'] != None and ret['Command'] != latest_command:
+                        latest_command = commands.pop()
+                        console.print(f' > {latest_command}', style='yellow')
+                        client.pub({'Command': latest_command}, verbose=debug_mode)
+                        sleep(.2)
+                    if len(commands) == 0:
+                        console.print('Finished!!', style='bold green')
+                        break
+                    sleep(tick_rate)
+        except KeyboardInterrupt:
+            client.pub({'AngularDistance':'','StopExplore':'','EntranceExit':'','Command':''}, verbose=debug_mode)
+        client.pub({'AngularDistance':'','StopExplore':'','EntranceExit':'','Command':''}, verbose=debug_mode)
+
+options = [
+        optparse.make_option('-s', '--solve', action='store_true', dest='solve_mode', help='solve the contracted maze or not', default=False),
+        optparse.make_option('-m', '--maze-size', type='int', dest='maze_size', help='size of the maze in blocks', default=6),
+        optparse.make_option('-b', '--block-length', type='int', dest='block_length', help='length of a single block', default=400),
+        optparse.make_option('-t', '--tick-rate', type='float', dest='tick_rate', help='wait for \'tick rate\' second to send another request to MQTT Broker', default=.1),
+        optparse.make_option('--show-maze', action='store_true', dest='show_maze', help='show constructed maze or not', default=False),
+        optparse.make_option('--dynamic-update-maze', action='store_true', dest='dynamic', help='show constructed maze dynamictically', default=False),
+        optparse.make_option('--debug', action='store_true', dest='debug', help='debug mode', default=False),
+    ]
+parser = optparse.OptionParser(option_list=options)
 
 if __name__ == '__main__':
-    main()
+    (options, args) = parser.parse_args()
+    run(
+        options.solve_mode, 
+        options.maze_size, 
+        options.block_length,
+        options.tick_rate,
+        options.show_maze, 
+        options.dynamic, 
+        options.debug)
--- src/Mapper.py
+++ src/Mapper.py
@@ -19,10 +19,11 @@ class Mapper:
         self.__cache_dir = 0
         self.x_glob = np.array([])
         self.y_glob = np.array([])
-        self.__maze_size = maze_size
-        self.__maze = np.zeros((self.__maze_size*2, self.__maze_size*2))
+        self.__maze_size = maze_size*2
+        self.__maze = np.zeros((self.__maze_size, self.__maze_size))
         self.__connected_points = []
         self.__padd = padd
+        self.__distance_rescaling_factor = 1
 
     @overload
     def map(self, forward_distance:int, rotate_angle:int, distance_rescaling_factor:int=1) -> None:
@@ -34,7 +35,11 @@ class Mapper:
 
     def map(self, a, b, c):
         if isinstance(a, int) and isinstance(b, int) and isinstance(c, int):
-            self.__connected_points.append((math.ceil(a/c), b))
+            self.__distance_rescaling_factor = c
+            if a < c-(c*.1): # 10% inaccurate
+                a = 0
+            # self.__connected_points.append((math.ceil(a/c), b)) # Forward direction algorithm
+            self.__connected_points.append((1 if a != 0 else a, b)) # One block direction algorithm
             return None
         elif isinstance(a, np.ndarray) and isinstance(b, np.ndarray) and isinstance(c, int):
             try:
@@ -90,31 +95,36 @@ class Mapper:
 
 
     def construct(self) -> np.ndarray:
+        # Construct maze from datapoint with One block direction algorithm
         if len(self.__connected_points) == 0:
             # return self.x_glob, self.y_glob
             return self.__maze
-        curr_x = self.__maze_size//2
-        curr_y = self.__maze_size//2
+        row = self.__maze_size//2
+        col = self.__maze_size//2
         sum_angle = 0
         for point in self.__connected_points:
-            if sum_angle == 360:
+            if sum_angle >= 360 or sum_angle <= -360:
                 sum_angle = 0
             sum_angle += point[1]
             if point[0] == 0:
+                self.__maze[row][col] += 1
                 continue
-            self.__maze[curr_x][curr_y] = 1
+            if np.all(self.__maze == 0):
+                self.__maze[row][col] += 1
             for _ in range(1, point[0]+1):
-                if sum_angle == 0 or sum_angle == 360:
-                    curr_x -= 1
-                elif sum_angle == 90:
-                    curr_y += 1
-                elif sum_angle == -90:
-                    curr_y -= 1
+                if sum_angle == 0 or sum_angle == 360 or sum_angle == -360:
+                    row -= 1
+                    # self.__maze[wall_x][wall_y] = 0
+                elif sum_angle == 90 or sum_angle == -270:
+                    col += 1
+                elif sum_angle == -90 or sum_angle == 270:
+                    col -= 1
                 elif sum_angle == 180 or sum_angle == -180:
-                    curr_x += 1
+                    row += 1
                 else:
-                    raise ValueError("Invalid rotate angle")
-                self.__maze[curr_x][curr_y] = 1
+                    raise ValueError("Invalid rotate angle : ", sum_angle)
+                self.__maze[row][col] += 1
+                # console.log(point, self.__maze, log_locals=True)
         # Trimming
         self.__maze = np.delete(self.__maze, np.where(~self.__maze.any(axis=0))[0], axis=1)
         self.__maze = np.delete(self.__maze, np.where(~self.__maze.any(axis=1))[0], axis=0)
@@ -122,3 +132,53 @@ class Mapper:
         if self.__padd:
             self.__maze = np.pad(self.__maze, pad_width=1, mode='constant', constant_values=0)
         return self.__maze
+
+    # def construct(self) -> np.ndarray:
+    #     if len(self.__connected_points) == 0:
+    #         # return self.x_glob, self.y_glob
+    #         return self.__maze
+    #     curr_x = self.__maze_size//2
+    #     curr_y = self.__maze_size//2
+    #     old_x = curr_x
+    #     old_y = curr_y
+    #     sum_angle = 0
+    #     for point in self.__connected_points:
+    #         if sum_angle == 360:
+    #             sum_angle = 0
+    #         sum_angle += point[1]
+    #         if point[0] == 0:
+    #             continue
+    #         if np.all(self.__maze == 0):
+    #             self.__maze[curr_x][curr_y] = 1
+    #         for _ in range(1, point[0]+1):
+    #             if abs(sum_angle) != 90 and self.__maze[old_x][old_y] > 0:
+    #                 backward_blocks = math.ceil(point[0]/self.__distance_rescaling_factor)
+    #                 for _ in range(backward_blocks):
+    #                     if sum_angle == 0 or sum_angle == 360 or sum_angle == -360:
+    #                         curr_x += 1
+    #                         old_x = curr_x+1
+    #                     elif sum_angle == 180 or sum_angle == -180:
+    #                         curr_x -= 1
+    #                         old_x = curr_x-1
+    #             else:
+    #                 old_x = curr_x
+    #                 old_y = curr_y
+    #                 if sum_angle == 0 or sum_angle == 360 or sum_angle == -360:
+    #                     curr_x -= 1
+    #                 elif sum_angle == 90:
+    #                     curr_y += 1
+    #                 elif sum_angle == -90:
+    #                     curr_y -= 1
+    #                 elif sum_angle == 180 or sum_angle == -180:
+    #                     curr_x += 1
+    #                 else:
+    #                     raise ValueError("Invalid rotate angle")
+    #                 self.__maze[curr_x][curr_y] = 1
+    #             console.log(self.__maze, log_locals=True)
+    #     # Trimming
+    #     self.__maze = np.delete(self.__maze, np.where(~self.__maze.any(axis=0))[0], axis=1)
+    #     self.__maze = np.delete(self.__maze, np.where(~self.__maze.any(axis=1))[0], axis=0)
+    #     # Padding
+    #     if self.__padd:
+    #         self.__maze = np.pad(self.__maze, pad_width=1, mode='constant', constant_values=0)
+    #     return self.__maze
