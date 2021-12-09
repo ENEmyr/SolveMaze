cc08109414d43ef24c10ccaa5f73b228b25eef3e
ENEmy <nosde@protonmail.com>
2021-12-09 09:00:17 +0700

    feat: add new function to support one block direction algorithm

--- src/Odometry.py
+++ src/Odometry.py
@@ -16,10 +16,12 @@ class Point:
 class Odometry:
     def __init__(self, angular_distances:Union[list, np.ndarray]=None) -> None:
         self.angular_dis = angular_distances
+
     def __str2list(self, lst_str:str) -> np.ndarray:
         lst = lst_str.replace('[', '').replace(']', '').split(',')
         nd_lst = (np.array(lst)).astype('int')
         return nd_lst
+
     def transform2cartesian(self, angular_distances:Union[str, List[int]]=None) -> list:
         angular_distances = self.angular_dis if angular_distances == None else angular_distances
         if angular_distances == None:
@@ -36,6 +38,19 @@ class Odometry:
             y = (100+(angular_distances[angle]/70)*sin(pi*angle/180+.13))*2
             points.append(Point(x, y))
         return points
+
+    def minimum_forward_distance(self, forward_distance:Union[str, List[int]]=None) -> tuple:
+        angular_distances = self.angular_dis if forward_distance == None else forward_distance
+        if forward_distance == None:
+            raise ValueError('Argument not found.')
+        if type(forward_distance) == str:
+            forward_distance = self.__str2list(forward_distance)
+        elif type(forward_distance) == list:
+            forward_distance = np.array(forward_distance).astype('int')
+        else:
+            raise ValueError('Wrong argument type.')
+        return (int(forward_distance[1]), int(forward_distance[0]))
+
     def maximum_forward_distance(self, angular_distances:Union[str, List[int]]=None) -> tuple:
         angular_distances = self.angular_dis if angular_distances == None else angular_distances
         if angular_distances == None:
