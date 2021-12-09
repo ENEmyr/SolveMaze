c307c020c0507b64a04ea4a427bd27e63c2bea2e
ENEmy <nosde@protonmail.com>
2021-12-09 08:59:45 +0700

    fix: skip new published data acording to number of subcribe topics

--- src/Client.py
+++ src/Client.py
@@ -22,7 +22,9 @@ class Client:
             password:str=MQTT_CONF.Password,
             qos:int=int(MQTT_CONF.Qos),
             keepalive:int=int(MQTT_CONF.Keepalive),
-            on_data_fnc:Callable=None):
+            on_data_fnc:Callable=None,
+            verbose:bool=False):
+        # TODO : Ignore first subcribe
         self.broker = broker
         self.port = port
         self.username = username
@@ -31,23 +33,27 @@ class Client:
         self.keepalive = keepalive
         self.subs = subs
         self.pubs = pubs
+        self.verbose = verbose
         self.client = mqtt.Client(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10)))
-        self.client.username_pw_set(self.username, self.password)
+        if self.username and self.password:
+            self.client.username_pw_set(self.username, self.password)
+        self.client.__init__
         self.client.connect(self.broker, self.port, self.keepalive)
         self.on_data_fnc = on_data_fnc if on_data_fnc != None else self.__on_data
         self.client.on_message = self.on_data_fnc
         self.subs_val = dict()
         self.cache_sub_ret = dict()
-        self.__first_sub = 0
+        # self.__first_sub = 0
         topics_lst = []
         for topic in self.subs.keys():
             topics_lst.append((self.subs[topic], self.qos))
-            self.__first_sub += 1
+            # self.__first_sub += 1
         [status, msg_id] = self.client.subscribe(topics_lst, self.qos)
         if len(self.subs) != 0:
             for ch in subs:
                 self.subs_val[ch.lower()] = None
-        console.log(f'Subscribe status: {status}, with message id: {msg_id}')
+        if self.verbose:
+            console.log(f'Subscribe status: {status}, with message id: {msg_id}')
 
     def sub(self, topics:list=[]) -> dict:
         ret_dict = dict()
@@ -62,15 +68,19 @@ class Client:
             elif topic.lower() in self.subs.values():
                 key = self.__get_subs_key(topic)
             else:
-                return ret_dict
-            ret_dict[topic] = self.subs_val[key]
-            if ret_dict[topic] != None:
-                ret_dict[topic] = ret_dict[topic].decode('utf-8')
+                continue
+            ret_dict[topic] = None
+            if self.subs_val[key] != None:
+                ret_dict[topic] = self.subs_val[key].decode('utf-8')
         if ret_dict == self.cache_sub_ret:
             for key in ret_dict:
                 ret_dict[key] = None
         else:
             self.cache_sub_ret = ret_dict.copy()
+        # if self.__first_sub > 0:
+            # self.__first_sub -= 1
+            # for key in ret_dict:
+                # ret_dict[key] = None
         return ret_dict
 
     def pub(self, topics_payload_pair:dict, verbose:bool=False) -> bool:
@@ -81,18 +91,17 @@ class Client:
                 topic_path = topic.lower()
             else:
                 topic_path = topic
-            result = self.client.publish(topic_path, topics_payload_pair[topic], self.qos, retain=False)
+            result = self.client.publish(topic_path, topics_payload_pair[topic], self.qos, retain=True)
             if verbose:
                 if result[0] == 0:
-                    print(f'Publish to topic: {topic} successfully.')
+                    if self.verbose:
+                        print(f'Publish to topic: {topic} successfully.')
                 else:
-                    print(f'Failed to publish to topic: {topic}, {result}.')
+                    if self.verbose:
+                        print(f'Failed to publish to topic: {topic}, {result}.')
         return True if result == 0 else False
 
     def __on_data(self, mosq, obj, msg):
-        if self.__first_sub > 0:
-            self.__first_sub -= 1
-            return
         payload = msg.payload
         topic = msg.topic.split('/')[3]
         if payload:
